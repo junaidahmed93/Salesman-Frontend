edd90d00fdbccc13d7cd5554cb642184886c1ae3
junaidahmed93@live.com <aries1993>
2016-01-29 17:54:43 +0500

    Before Adding Order

---
+++ www/js/app.js
@@ -0,0 +1,58 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var app=angular.module('starter', ['ionic']);
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  })
+});
+
+app.config(function($stateProvider,$urlRouterProvider,$httpProvider){
+    $stateProvider
+    .state("login",{
+        url:"/login",
+        templateUrl:"view/login.html",
+        controller:"loginCtrl"
+    })
+    .state("home",{
+        url:"/home",
+        templateUrl:"view/home.html",
+        controller:"homeCtrl"
+    })
+    .state("addRetailer",{
+        url:"/addRetailer",
+        templateUrl:"view/addRetailer.html",
+        controller:"addRetailerCtrl"
+    });
+    $urlRouterProvider.otherwise("login");
+});
+
+
+
+app.controller("loginCtrl",function(){
+    console.log("i am in login ctrl");
+});
+
+app.controller("homeCtrl",function(){
+   console.log("home"); 
+});
+
+app.controller("addRetailerCtrl",function(){
+    console.log("addRetailer");
+});
